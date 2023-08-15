aee0a0433db94bff287e72e69f7fced863e3419a
R-Saunders <richsaunders@protonmail.com>
2023-08-15 14:29:22 +0100

    more work on lab 4

--- lab-4/src/app/hobbies/[hobbie]/page.tsx
+++ lab-4/src/app/hobbies/[hobbie]/page.tsx
@@ -1,8 +1,25 @@
 type HobbiePageParam = {
-  hobbie: string
-}
+	hobbie: string;
+};
+
+type TTG = {
+	name: string;
+};
+
+const TTGS: TTG[] = [
+	{ name: "Dungeons & Dragons" },
+	{ name: "Agricola" },
+	{ name: "Wingspan" },
+	{ name: "Betrayal at Baldur's Gate" },
+	{ name: "Azul" },
+];
 
 export default function HobbiePage({ params }: { params: HobbiePageParam }) {
-  console.log(params);
-  return <p>This is the individual pet page for {params.hobbie}</p>;
+	console.log(params);
+
+	return (
+		<main>
+			<h1>Welcome to my page all about {params.hobbie}</h1>
+		</main>
+	);
 }
