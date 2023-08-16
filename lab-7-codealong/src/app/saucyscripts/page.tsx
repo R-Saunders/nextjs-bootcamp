1b134e6ecac03cb49c68c6ea488565c2bbd9734a
R-Saunders <richsaunders@protonmail.com>
2023-08-16 13:07:10 +0100

    fixed script src in saucyscripts

--- lab-7-codealong/src/app/saucyscripts/page.tsx
+++ lab-7-codealong/src/app/saucyscripts/page.tsx
@@ -1,10 +1,10 @@
-import Script from "next/script"
+import Script from "next/script";
 
 export default function SaucyScripts() {
-  return (
-    <div>
-      <h2>Saucy Scripts</h2>
-<Script src="https://www.example.com/scripts"></Script>
-    </div>
-  )
+	return (
+		<div>
+			<h2>Saucy Scripts</h2>
+			<Script src="https://www.example.com/script.js"></Script>
+		</div>
+	);
 }
